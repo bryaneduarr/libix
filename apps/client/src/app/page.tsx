2a1e96fee9c2e2012faa5df78c38b5d48c3f962e
Oscarmrl <oscaralexandermurillo500@gmail.com>
2025-06-18 15:11:35 -0600

    Refactor About page, add Products page, update Navigation component with dropdown menu, and adjust TypeScript configuration

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Page() {
   return (
-    <div className="relative min-h-screen">
+    <div className="relative min-h-screen pt-20">
       <div
         className="absolute inset-0 -z-10 bg-cover bg-center bg-no-repeat"
         style={{
@@ -9,6 +9,13 @@ export default function Page() {
       ></div>
 
       <div className="absolute inset-0 -z-10 bg-black opacity-40"></div>
+
+      <div className="grid min-h-screen grid-cols-2">
+        <div className="text-center text-white">
+          <h1 className="mb-4 text-4xl font-bold">Welcome to Our App</h1>
+          <p className="text-lg">This is a simple landing page.</p>
+        </div>
+      </div>
     </div>
   );
 }
--- apps/client/src/components/Navigation.tsx
+++ apps/client/src/components/Navigation.tsx
@@ -1,88 +1,91 @@
 "use client";
 
-import React, { useState } from "react";
+import Link from "next/link";
 import { ModeToggle } from "@/components/mode-toggle";
 import { BookOpen, House, Menu, ShoppingCart, UsersRound } from "lucide-react";
 
-export default function Navigation() {
-  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@workspace/ui/components/dropdown-menu";
 
+export default function Navigation() {
   return (
     <div>
       <nav className="bg-secondary text-foreground fixed left-0 top-0 z-50 w-full p-4">
         <div className="flex items-center justify-between">
           <div className="text-2xl">svg</div>
 
-          <div className="flex items-center space-x-4 sm:hidden">
-            <button onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}>
-              <Menu size={24} />
-            </button>
-
-            <ModeToggle />
-          </div>
-
           <ul className="hidden items-center space-x-5 text-sm font-bold sm:flex">
             <li>
-              <a href="/" className="flex items-center gap-2">
+              <Link href="/" className="flex items-center gap-2">
                 <House size={20} />
                 Home
-              </a>
+              </Link>
             </li>
             <li>
-              <a href="/about" className="flex items-center gap-2">
+              <Link href="/about" className="flex items-center gap-2">
                 <UsersRound size={20} />
                 About
-              </a>
+              </Link>
             </li>
             <li>
-              <a href="/products" className="flex items-center gap-2">
+              <Link href="/products" className="flex items-center gap-2">
                 <BookOpen size={20} />
                 Products
-              </a>
+              </Link>
             </li>
             <li>
-              <a href="/contact" className="flex items-center gap-2">
+              <Link href="/contact" className="flex items-center gap-2">
                 <ShoppingCart size={20} />
                 Cart
-              </a>
+              </Link>
             </li>
             <li className="flex items-center">
               <ModeToggle />
             </li>
           </ul>
+
+          {/* Mobile menu button */}
+          <div className="flex items-center gap-2 sm:hidden">
+            <ModeToggle />
+
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <button className="p-2">
+                  <Menu size={24} />
+                </button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="w-48">
+                <DropdownMenuItem>
+                  <Link href="/" className="flex items-center gap-2">
+                    <House size={10} />
+                    Home
+                  </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <Link href="/about" className="flex items-center gap-2">
+                    <UsersRound size={10} /> About
+                  </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <Link href="/products" className="flex items-center gap-2">
+                    <BookOpen size={10} /> Products
+                  </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <Link href="/#" className="flex items-center gap-2">
+                    <ShoppingCart size={10} />
+                    Cart
+                  </Link>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
         </div>
       </nav>
-
-      {isMobileMenuOpen && (
-        <div className="text-foreground bg-secondary fixed left-2/3 top-6 z-50 mt-12 w-40 rounded-bl-lg sm:hidden">
-          <ul className="flex flex-col justify-center space-y-3 text-sm font-bold md:hidden">
-            <li>
-              <a href="/" className="flex items-center gap-2">
-                <House size={20} />
-                Home
-              </a>
-            </li>
-            <li>
-              <a href="/about" className="flex items-center">
-                <UsersRound size={20} />
-                About
-              </a>
-            </li>
-            <li>
-              <a href="/products" className="flex items-center">
-                <BookOpen size={20} />
-                Products
-              </a>
-            </li>
-            <li>
-              <a href="/contact" className="flex items-center">
-                <ShoppingCart size={20} />
-                Cart
-              </a>
-            </li>
-          </ul>
-        </div>
-      )}
     </div>
   );
 }
